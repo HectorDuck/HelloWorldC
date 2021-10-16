b78352d48906c16de1d0d5ea87a1055841ca9c3d
EmptySpace <50351002+xOlive24@users.noreply.github.com>
2021-10-15 20:40:56 -0700

    Add files via upload

---
+++ HelloWorldC/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HelloWorldC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
